6d2b32683da2afdd0cbdc34f062e0112040aed1a
Richard Cartwright <spark@streampunk.media>
2017-05-19 16:18:07 +0100

    Fix command line options for join group.

--- scratch/join_group.js
+++ scratch/join_group.js
@@ -17,20 +17,16 @@ var dgram = require('dgram');
 var argv = require('yargs')
   .default('p', 6789)
   .default('a', '234.5.6.7')
-  .demandOption(['i'])
   .number('p')
   .usage('Join a multicast group. Useful for wireshark testing.\n' +
     'Usage: $0 ')
   .help()
   .describe('a', 'Address of multicast group to join.')
-  .describe('g', 'Port of multicast group to join.')
+  .describe('p', 'Port of multicast group to join.')
   .describe('i', 'Multicast interface.')
   .argv;
 
 var socket = dgram.createSocket('udp4');
-
-var addr = process.argv[2];
-var port = +process.argv[3];
 
 socket.addMembership(argv.a, argv.i);
 
